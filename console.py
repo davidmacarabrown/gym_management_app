1ec41c0f519b039efee302301450015232474af0
David Macara Brown <davidmacarabrown@gmail.com>
2021-07-10 16:55:10 +0100

    tables inserted and database running

---
+++ console.py
@@ -0,0 +1,8 @@
+from models.member import Member
+from models.gym_class import Gym_Class
+from models.booking import Booking
+
+import repositories.member_repository as member_repository
+import repositories.gym_class_repository as gym_class_repository
+import repositories.booking_repository as booking_repository
+
--- db/gym_database.sql
+++ db/gym_database.sql
@@ -7,15 +7,16 @@ CREATE TABLE member(
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255)
-)
+);
 
 CREATE TABLE class (
     id SERIAL PRIMARY KEY,
-    class_name VARCHAR(255)
-)
+    class_name VARCHAR(255),
+    class_description TEXT
+);
 
 CREATE TABLE bookings (
     id SERIAL PRIMARY KEY,
-    class_id REFERENCES class(id),
-    member_id REFERENCES member(id)
-)
+    class_id INT REFERENCES class(id) ON DELETE CASCADE,
+    member_id INT REFERENCES member(id) ON DELETE CASCADE
+);
