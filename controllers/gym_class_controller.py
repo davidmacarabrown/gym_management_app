675fd6fa42c3be84680bd7dce35b422f4d0d94af
David Macara Brown <davidmacarabrown@gmail.com>
2021-07-11 23:26:49 +0100

    working on classes controller and routes

--- controllers/gym_class_controller.py
+++ controllers/gym_class_controller.py
@@ -1,6 +1,13 @@
 from flask import Flask, Blueprint, render_template, request, redirect
 
+import repositories.gym_class_repository as gym_class_repository
+
 from models.gym_class import GymClass
 
 gym_class_blueprint = Blueprint("gym_class", __name__)
+
+@gym_class_blueprint.route("/classes")
+def classes():
+    all_classes = gym_class_repository.select_all()
+    return render_template("/gym_class/index.html", all_classes = all_classes)
 
