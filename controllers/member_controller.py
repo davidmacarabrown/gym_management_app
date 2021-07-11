f993e9cb6cfd8776f5f7999089800606d5e66191
David Macara Brown <davidmacarabrown@gmail.com>
2021-07-11 17:40:01 +0100

    list of members displayed on /members page

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -1,4 +1,5 @@
 from flask import Flask, Blueprint, blueprints, render_template, request, redirect
+import repositories.member_repository as member_repository
 
 from models.member import Member
 
@@ -6,7 +7,8 @@ members_blueprint = Blueprint("members", __name__)
 
 @members_blueprint.route("/members")
 def members():
-    return render_template("/members/index.html")
+    all_members = member_repository.select_all()
+    return render_template("/members/index.html", all_members = all_members)
 
 @members_blueprint.route("/members/new")
 def new():
