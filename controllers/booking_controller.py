f246bddca9f36947c8c3877f0e6f6da14bbadb94
David Macara Brown <davidmacarabrown@gmail.com>
2021-07-12 14:01:15 +0100

    /bookings index.html added and route in place to display the page

--- controllers/booking_controller.py
+++ controllers/booking_controller.py
@@ -3,3 +3,7 @@ from flask import Flask, Blueprint, render_template, request, redirect
 from models.booking import Booking
 
 bookings_blueprint = Blueprint("booking", __name__)
+
+@bookings_blueprint.route("/bookings")
+def bookings():
+    return render_template("/bookings/index.html")
